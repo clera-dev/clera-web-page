9eae058d54d5b5bb4357723ef16021154b78fe5e
Cristian Mendoza <cmendoza1031@outlook.com>
2025-03-09 17:38:38 -0700

    Fix overscroll white background issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
-      <body className={`${inter.variable} font-sans antialiased`}>
+    <html lang="en" className="dark bg-black">
+      <body className={`${inter.variable} font-sans antialiased bg-black`}>
         <div id="top"></div>
         <Navigation />
         {children}
